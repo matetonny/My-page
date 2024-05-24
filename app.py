c6b150416bae2c476b0a05f1d8b5489e15eddef9
Mate Chocheli <matechocheli2@gmail.com>
2024-05-24 19:11:23 +0400

    switched to pywsgi server

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from views import views
 
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix='/')
-run_with_ngrok(app)
 
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     from waitress import serve
+#     serve(app, host='0.0.0.0', port=3333)  
