95eaad63a05510bdc38ab3c193373fb4dbdeda24
Mate Chocheli <matechocheli2@gmail.com>
2024-05-27 22:45:19 +0400

    made the intro

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # My page
-A page that i made with python(flask), html and css.
+
+This is a website i made using a python(with the flask library)
+backend and HTML, CSS and JS frontend. Here you can see my projects
+and some useful utilities. You can find it on the URL: <https://mtonny.dev>
---
+++ static/js/header.js
@@ -0,0 +1,34 @@
+let navbar = document.querySelector("#navbar");
+let hamburgerMenu = document.querySelector("#hamburgerMenu");
+let hamburgerMenuBtn = document.querySelector("#hamburgerMenuBtn");
+let header = document.querySelector("header");
+let hamburgerMenuIcon = document.querySelector("#hamburgerMenuIcon");
+
+function check_size(){
+    if (window.innerWidth <= 650){
+        navbar.classList.add("hidden");
+        hamburgerMenuBtn.classList.remove("hidden");
+    } else {
+        navbar.classList.remove("hidden");
+        hamburgerMenuBtn.classList.add("hidden");
+        hamburgerMenu.classList.add("folded");
+        hamburgerMenu.classList.remove("unfolded");
+    }
+}
+check_size();
+window.addEventListener("resize", check_size);
+
+let counter = 0;
+hamburgerMenuBtn.addEventListener("click", function(){
+    hamburgerMenu.classList.toggle("folded");
+    hamburgerMenu.classList.toggle("unfolded");
+
+    if (counter % 2 == 0){
+        hamburgerMenuIcon.setAttribute("src", "/static/images/x-symbol.svg");
+        hamburgerMenuIcon.setAttribute("width", "30");
+    } else {
+        hamburgerMenuIcon.setAttribute("src", "/static/images/burger-menu.svg");
+        hamburgerMenuIcon.setAttribute("width", "40");
+    }
+    counter++;
+})
